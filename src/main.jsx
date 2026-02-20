84cfdb178bdc47fa6b44dc74d88c2fecc5c74f86
Curtis197 <curtiscapre@gmail.com>
2026-02-20 15:58:02 +0100

    add vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import KelendOffre from "../Kelen_Offre.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <KelendOffre />
+  </StrictMode>
+);
